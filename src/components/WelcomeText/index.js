631df91f583cd7774b9089cc8a535461a9e8e17b
kesyavlad <kesyavlad74@gmail.com>
2022-10-24 10:01:04 +0300

    Fix style Welcome Text

--- src/components/WelcomeText/index.js
+++ src/components/WelcomeText/index.js
@@ -1,8 +1,28 @@
 import React from "react";
-import { Container, Typography } from "@mui/material";
+import {
+  Container,
+  createTheme,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
 
 const WelcomeText = () => {
   const welcomeText = "You need to make a choice Todos or Images";
+  const theme = createTheme();
+
+  theme.typography.h3 = {
+    fontSize: "1.2rem",
+    "@media (min-width:600px)": {
+      fontSize: "1.5rem",
+    },
+    "@media (max-width:415px)": {
+      fontSize: "1rem",
+    },
+
+    [theme.breakpoints.up("md")]: {
+      fontSize: "2rem",
+    },
+  };
   const style = {
     text: {
       wordBreak: "break-all",
@@ -13,9 +33,11 @@ const WelcomeText = () => {
   };
   return (
     <Container maxWidth="md">
-      <Typography variant="h4" style={style.text}>
-        {welcomeText}
-      </Typography>
+      <ThemeProvider theme={theme}>
+        <Typography variant="h3" style={style.text} mt={3}>
+          {welcomeText}
+        </Typography>
+      </ThemeProvider>
     </Container>
   );
 };
